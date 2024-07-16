861a3fdf70fa59de2b2d2e579b219ab5ac718353
Andrew Lee <andrewjlee131@gmail.com>
2024-07-15 21:18:12 -0400

    Prevent coaches/admin from purchasing session

--- src/components/views/SessionList.js
+++ src/components/views/SessionList.js
@@ -6,21 +6,33 @@ const SessionList = (props) => {
     
     const listingId = props.listingId;
     const[fetched, setFetched] = useState(false);
+    const [notCustomer, setNotCustomer] = useState(false);
+    const[addedSession, setAddedSession] = useState('');
     const sessions = props.sessions;
     
    function addToCart(customerId, sessionDate, session){
+    if(sessionStorage.getItem('role') === 'CUSTOMER'){
     SessionService.containsParticipant(session?.id, customerId).then(res => {
         if(res.data === true){
             props.updateErrorDate(sessionDate);
         }else{
             props.addSessionToCart(session);
+            updateAddedSession(session);
         }
     })
+    }else{
+        setNotCustomer(true);
+    }
     
+   }
+   function updateAddedSession(session){
+    setAddedSession(session);
    }
 
     return(
         <div className = "session-list">
+            {notCustomer && <p>Customer account is required to purchase sessions.</p> }
+            {addedSession !== '' && <p>Session - {addedSession} added to cart!</p>}
             {sessions?.map((session) => (
             <div className="session-info" key = {session?.id}>
                 <p>Date: {session?.date}</p>
